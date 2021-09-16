ed3dc77e704bee257227a906ccd7cd776f0a8282
Toarexer <58662537+Toarexer@users.noreply.github.com>
2021-09-16 08:20:40 +0200

    bug fixes

--- snake.c
+++ snake.c
@@ -7,7 +7,7 @@
 
 uint record_score;
 char map[MAP_HEIGHT * MAP_WIDTH];
-char direction = D_UP;
+char direction = D_UP, next_direction = D_UP;
 bool input_thread_running = true;
 
 struct snake_s
@@ -26,22 +26,16 @@ struct fruit_s
     char present;
 } fruit;
 
-void create_map()
-{
-    for (uint y = 0; y < MAP_HEIGHT; y++)
-        for (uint x = 0; x < MAP_WIDTH; x++)
-            map[mappos(x, y)] = (y == 0 || y == MAP_HEIGHT - 1 || x == 0 || x == MAP_WIDTH - 1) ? '#' : ' ';
-}
-
 void draw_map()
 {
     printf("\e[H");
     for (uint y = 0; y < MAP_HEIGHT; y++)
     {
         for (uint x = 0; x < MAP_WIDTH; x++)
-            putchar(map[mappos(x, y)]);
+            putchar((y == 0 || y == MAP_HEIGHT - 1 || x == 0 || x == MAP_WIDTH - 1) ? '#' : ' '); // ▒
         printf("\n\e[G");
     }
+
     if (fruit.present)
         printf("\e[%u;%uH\e[91m@\e[0m\n", fruit.y + 1, fruit.x + 1);
 }
@@ -64,7 +58,7 @@ void move_snake()
 {
     for (uint i = snake.length - 1; i > 0; i--)
         snake.body[i] = snake.body[i - 1];
-    switch (direction)
+    switch (direction = next_direction)
     {
     case D_UP:
         snake.body[0].y--;
@@ -81,10 +75,10 @@ void move_snake()
     }
 }
 
-bool body_collision()
+bool body_collision(uint x, uint y)
 {
     for (uint i = 1; i < snake.length; i++)
-        if (snake.body[0].x == snake.body[i].x && snake.body[0].y == snake.body[i].y)
+        if (x == snake.body[i].x && y == snake.body[i].y)
             return true;
     return false;
 }
@@ -100,13 +94,13 @@ void *getinput(void *args)
             return NULL;
         }
         else if (c == 'w' && direction != D_DOWN)
-            direction = D_UP;
+            next_direction = D_UP;
         else if (c == 'a' && direction != D_RIGHT)
-            direction = D_LEFT;
+            next_direction = D_LEFT;
         else if (c == 's' && direction != D_UP)
-            direction = D_DOWN;
+            next_direction = D_DOWN;
         else if (c == 'd' && direction != D_LEFT)
-            direction = D_RIGHT;
+            next_direction = D_RIGHT;
         else if (c == '+')
         {
             snake.length++;
@@ -139,7 +133,6 @@ int main()
 
     printf("\e[?1049h\e[?25l"); // enable alternative screen buffer and hide cursor
 
-    create_map();
     snake.length = SNAKE_STARTLEN;
     for (uint i = 0; i < snake.length; i++)
     {
@@ -172,7 +165,7 @@ int main()
             break;
         }
 
-        if (body_collision())
+        if (body_collision(snake.body[0].x, snake.body[0].y))
         {
             printf("\e[%u;%uH\e[31m\e[5mO\e[0m\n", snake.body[0].y + 1, snake.body[0].x + 1);
             break;
@@ -183,10 +176,13 @@ int main()
         if (fruit.cooldown == 0)
         {
             time_t t;
-            srand(time(&t));
-            fruit.x = rand() % (MAP_WIDTH - 2) + 1;
-            srand(fruit.x);
-            fruit.y = rand() % (MAP_HEIGHT - 2) + 1;
+            do
+            {
+                srand(time(&t));
+                fruit.x = rand() % (MAP_WIDTH - 2) + 1;
+                srand(fruit.x);
+                fruit.y = rand() % (MAP_HEIGHT - 2) + 1;
+            } while (body_collision(fruit.x, fruit.y));
             fruit.cooldown = FRUIT_MAXC;
             fruit.present = true;
         }
